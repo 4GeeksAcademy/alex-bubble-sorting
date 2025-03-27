46f1b13647e56f35755e8ff4b94e9c7ac61a7fb6
Alexander Morales <alexandermorales.n1@gmail.com>
2025-03-27 05:11:07 +0000

    generate randomCard function, add html and css

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numElement = document.querySelector('#numberElement')
+  let suiteTop = document.querySelector('#suite-top')
+  let suiteBottom = document.querySelector('#suite-bottom')
+
+  let nums = ['A', 'J', 'Q', 'K', 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let suites = ['clubs', 'spades', 'heart', 'diamonds']
+
+  let generateCard = function () {
+    let num = nums[Math.floor(Math.random() * nums.length)]
+    let suit = suites[Math.floor(Math.random() * suites.length)]
+
+    numElement.textContent = num;
+    suiteTop.src = `assets/img/${suit}.png`
+    suiteBottom.src = `assets/img/${suit}.png`
+
+  }
+
+  generateCard()
 };
